4abd416d8d03e97433e232a5dfdd65486cdda694
BurcisWolf <furcis.burcis@gmail.com>
2024-09-19 13:08:11 +0200

    Played with JSX in React, defined a varible and called it in HTML, also learned about HTML attributes and JSX specifict atributs

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
-const title = 'React!';
+// Defining JavaScript Object
+const welcome = {
+  greeting: "Hello",
+  name: "React!",
+};
+
+//Defining JavaScript Funktion
+function welcomeMessage(name){
+  return "Welcome in our course, lets be best Mr./ Mrs. " + name;
+}
+
+const title = "React";
+const myElement = <h2>Hello {title}</h2>; // JSX 
 
 function App() {
 
   return (
     <div>
-      <h1>Hello {title}</h1>
+      <h1>{welcome.greeting} {welcome.name}</h1>
+      <p>{welcomeMessage("React")}</p>
+        <label htmlFor="search">Search: </label>
+        <input id="search" type="text"/>
+    {myElement}
+
     </div>
   )
 }
