f9e27e61c0cc57cedc268fbf856d6b6dc9b412ce
BurcisWolf <furcis.burcis@gmail.com>
2024-09-19 22:15:59 +0200

    Playgournd with map() JSX, its interesting how we can iterate thru whole list of times in an array

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,28 @@ function welcomeMessage(name){
 
 const title = "React";
 const myElement = <h2>Hello {title}</h2>; // JSX 
+const number = [1, 2, 4, 8, 16, 32];
+
+const exponentialNumbers = number.map(function (number) {
+  return number * number;
+});
+
+const list = [{
+  title: 'React',
+  url: 'test.org',
+  author: 'Me',
+  num_comments: 4,
+  points: 4,
+  objectID: 0,
+},
+{
+  title: 'Angular',
+  url: 'testo.org',
+  author: 'him',
+  num_comments: 2,
+  points: 2,
+  objectID: 1,
+}];
 
 function App() {
 
@@ -21,7 +43,18 @@ function App() {
         <label htmlFor="search">Search: </label>
         <input id="search" type="text"/>
     {myElement}
-
+    <hr />
+    <ul>
+      {list.map(function (item) {
+        return (
+        <li key={item.objectID}>
+          <span><a href={item.url}>{item.title}</a> </span>
+          <span>Author: {item.author} </span>
+          <span>Points: {item.points} </span>
+          <span>Number of commnents: {item.num_comments}</span>
+          </li>)
+      })}
+    </ul>
     </div>
   )
 }
