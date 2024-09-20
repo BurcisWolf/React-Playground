a52e0f490da97568d030f07a142127e463bdd002
BurcisWolf <furcis.burcis@gmail.com>
2024-09-20 12:46:01 +0200

    Playinng with funktions and calling whole HTML from funktions

--- src/App.jsx
+++ src/App.jsx
@@ -17,44 +17,58 @@ const exponentialNumbers = number.map(function (number) {
   return number * number;
 });
 
+// Defining JavaScript array with more stuff inside ( Real life list )
 const list = [{
   title: 'React',
-  url: 'test.org',
-  author: 'Me',
+  url: 'www.test.org',
+  author: 'Jemand',
+  about: "This article is about React",
   num_comments: 4,
   points: 4,
   objectID: 0,
 },
 {
   title: 'Angular',
-  url: 'testo.org',
-  author: 'him',
+  url: 'www.testo.org',
+  author: 'Jemand Anderes',
+  about: "This Article is about Angular",
   num_comments: 2,
   points: 2,
   objectID: 1,
 }];
-
+// FUnktion for our list so we dont need to edit App()
+function List() {
+  return (
+    <ul>
+    {list.map(function (item) {
+      return (
+      <li key={item.objectID}>
+        <span><a href={item.url}>{item.title}</a> |</span>
+        <span> About: {item.about} |</span>
+        <span> Author: {item.author} |</span>
+        <span> Points: {item.points} |</span>
+        <span> Number of commnents: {item.num_comments}</span>
+        </li>
+        )
+    })}
+    </ul>
+  )}
+// Funktion for our search so we dont need to edit App()
+function Search() {
+  return (  
+    <div>    
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text"/>
+    </div>  
+  )
+}
 function App() {
-
   return (
     <div>
       <h1>{welcome.greeting} {welcome.name}</h1>
-      <p>{welcomeMessage("React")}</p>
-        <label htmlFor="search">Search: </label>
-        <input id="search" type="text"/>
-    {myElement}
+      <Search />
     <hr />
-    <ul>
-      {list.map(function (item) {
-        return (
-        <li key={item.objectID}>
-          <span><a href={item.url}>{item.title}</a> </span>
-          <span>Author: {item.author} </span>
-          <span>Points: {item.points} </span>
-          <span>Number of commnents: {item.num_comments}</span>
-          </li>)
-      })}
-    </ul>
+      <List />
     </div>
   )
 }
